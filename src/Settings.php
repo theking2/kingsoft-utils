8b99f31d1b4e270d5b7c4ae8d18aa34fa4443cc7
Johannes <theking2@king.ma>
2023-06-13 16:21:44 +0200

    Fixes #3
    check for SETTINGS set

--- src/Settings.php
+++ src/Settings.php
@@ -1,14 +1,17 @@
 <?php declare(strict_types=1);
 namespace Kingsoft\Utils;
 
-if(!defined('SETTINGS_FILE')) {
-  define('SETTINGS_FILE', $_SERVER['DOCUMENT_ROOT'] . '/settings.ini');
-};
-
-class Settings{
-  public static function getSettings(): array {
-    return parse_ini_file(SETTINGS_FILE, true);
+if( !defined( SETTINGS ) ) {
+  if( !defined( 'SETTINGS_FILE' ) ) {
+    define( 'SETTINGS_FILE', $_SERVER[ 'DOCUMENT_ROOT' ] . '/settings.ini' );
   }
-}
+  class Settings
+  {
+    public static function getSettings(): array
+    {
+      return parse_ini_file( SETTINGS_FILE, true );
+    }
+  }
 
-define('SETTINGS', Settings::getSettings());
+  define( 'SETTINGS', Settings::getSettings() );
+}
