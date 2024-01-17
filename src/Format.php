39c9f11acbd47156ac4b7a59a4e596de196f93ad
Johannes <theking2@king.ma>
2024-01-17 14:50:57 +0100

    Use classes for formatters and web utils Fixes #8

---
+++ src/Format.php
@@ -0,0 +1,175 @@
+<?php declare(strict_types=1);
+
+namespace Kingsoft\Utils;
+
+define( 'DEBUG', strpos( $_SERVER['SERVER_NAME'], 'localhost' ) !== false );
+class Format
+{
+	/**
+	 * use {$_const('DEFINED')} to interpolate constants ( Example "Debug is: {$_const('DEBUG')}\n" )
+	 */
+	public static $__const = 'constant';
+
+	/**
+	 * Are we in debug mode?
+	 */
+
+	/**
+	 * calculate difference in procent
+	 * @param float $a numerator
+	 * @param float $b denominator 
+	 * @return string result in procent with one decimal or NaN if donominator is zero
+	 */
+	public static function proc_diff( $a, $b ): string
+	{
+		return $b != 0
+			? number_format( 100.0 * ( $a - $b ) / $b, 1, '.', "’" ) . '%'
+			: 'n.a.';
+	}
+
+	/**
+	 * format a money float
+	 * @param float $value - value to convert
+	 * @return string
+	 */
+	public static function money( $value ): string
+	{
+		return is_numeric( $value )
+			? number_format( $value, 2, '.', "’" )
+			: '';
+	}
+	/**
+	 * format a money float
+	 * @param float $value - value to convert
+	 * @return string
+	 */
+	public static function moneyz( $value ): string
+	{
+		return is_numeric( $value )
+			? number_format( $value, 2, '.', "’" )
+			: '0.00';
+	}
+	/**
+	 * format a percent float
+	 * @param float $value - value to convert
+	 * @return string
+	 */
+	public static function percent( float $value ): string
+	{
+		return is_numeric( $value )
+			? number_format( $value, 1, '.', "’" ) . '%'
+			: 'n.a.';
+	}
+	/**
+	 * format a percent float
+	 * @param float $value - value to convert
+	 * @return string
+	 */
+	public static function percentz( $value ): string
+	{
+		return is_numeric( $value )
+			? number_format( $value, 1, '.', "’" ) . '%'
+			: 'n.a.';
+	}
+	/**
+	 * format a liter float
+	 * @param float $value - value to convert
+	 * @return string
+	 */
+	public static function liter( float $value ): string
+	{
+		return is_numeric( $value )
+			? number_format( $value, 3, '.', "’" )
+			: $value;
+	}
+	/**
+	 * format a amount float/integer, floats get one decimal
+	 * @param float $value - value to convert
+	 * @return string
+	 */
+	public static function amount( $value ): string
+	{
+		return is_float( $value )
+			? number_format( $value, 1, '.', "’" )
+			: ( is_numeric( $value )
+				? number_format( $value, 0, '.', "’" )
+				: '' );
+	}
+
+	/**
+	 * Custom fputcsv
+	 * @param resource $handle filehandle
+	 * @param mixed[] $fields array of values to write
+	 * @param string $delimiter field delimiter
+	 * @param string $enclosure field enclosures
+	 * @param string $escape_char escape enclosure chars in fields
+	 * @param string $record_seperator 
+	 * @return int characters written
+	 */
+	public static function fputcsv(
+	  $handle,
+		array $fields,
+		string $delimiter = ",",
+		string $enclosure = '"',
+		string $escape_char = "\\",
+		string $record_seperator = "\r\n"
+	) {
+		$result = [];
+		foreach( $fields as $field ) {
+			$result[] = $enclosure . str_replace( $enclosure, $escape_char . $enclosure, $field ) . $enclosure;
+		}
+		return fwrite( $handle, implode( $delimiter, $result ) );
+	}
+
+	/**
+	 * get array value if key exist otherwise default
+	 * @param string $key needle
+	 * @param mixed[] $array haystack
+	 * @param mixed $default value if needle not found
+	 */
+	public static function array_value( string $key, string $array, ?string $default = '' ): string
+	{
+		return $array[ $key ] ?? $default;
+	}
+
+	/**
+	 * Convert kebab-case to PascalCase
+	 * @param string $str
+	 * @return string
+	 */
+	public static function kebabToPascal( string $str ): string
+	{
+		return str_replace( ' ', '', ucwords( str_replace( '-', ' ', $str ) ) );
+	}
+
+	/**
+	 * Convert snake_case to PascalCase
+	 * @param string $str
+	 * @return string
+	 */
+	public static function snakeToPascal( string $str ): string
+	{
+		return str_replace( ' ', '', ucwords( str_replace( '_', ' ', $str ) ) );
+	}
+
+	/**
+	 * Convert snake_case to camelCase
+	 * @param string $str
+	 * @return string
+	 */
+	public static function snakeToCamel( string $str ): string
+	{
+		return lcfirst( snakeToPascal( $str ) );
+	}
+
+	/**
+	 * Convert kebab-case to camelCase
+	 * @param string $str
+	 * @return string
+	 */
+	public static function kebabToCamel( string $str ): string
+	{
+		return lcfirst( kebabToPascal( $str ) );
+	}
+
+}
---
+++ src/Html.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types=1);
+
+namespace Kingsoft\Utils;
+
+class Html
+{
+  /**
+   * check_params
+   *
+   * @param  array $params this params should be in the $_REQUEST
+   * @return bool true when all params are in $_REQUEST
+   */
+  static public function check_request_params( array $params ): bool
+  {
+    $params_check = array_intersect( array_keys( $_REQUEST ), $params );
+    return count( $params_check ) !== count( $params );
+  }
+
+  /**
+   * convert bin to url friendly base64
+   * translating + to - and / to _
+   * @param string $data
+   * @return string
+   */
+  static public function base64url_encode( $data ): string
+  {
+    return rtrim( strtr( base64_encode( $data ), '+/', '-_' ), '=' );
+  }
+  /**
+   * convert url friendly base64 to bin
+   * translating - to + and _ to /
+   * @param string $data
+   * @return string
+   */
+  static public function base64url_decode( $data ): string
+  {
+    return base64_decode( strtr( $data, '-_', '+/' ) . str_repeat( '=', 3 - ( 3 + strlen( $data ) ) % 4 ) );
+  }
+
+  /**
+   * Wrap a text in a tag
+   * @param string $tag Tag to wrap arount text
+   * @param string $text Text to wrap tag around
+   * @param ?string $class optional class or classes string
+   * @param ?string $id optional id string
+   * @return string
+   */
+  static public function wrap_tag( string $tag, string $text, ?string $class = null, ?string $id = null ): string
+  {
+    return
+      "<$tag" .
+        // if class is set include a class="" section
+      ( $class ? " class=\"$class\"" : '' ) .
+
+        // if id is set include a id="" section
+      ( $id ? " id=\"$id\"" : '' ) .
+
+      ">$text</$tag>";
+  }
+
+  /**
+   * Create option entry setting the selected value
+   *
+   * @param string $text Text to display
+   * @param string $value the value attribute of the option
+   * @param string $var The variable holding $value to test the selection
+   * @return string
+   */
+  static public function option_tag( string $text, mixed $value, string $var ): string
+  {
+    return
+      '<option ' .
+        // set attribute selected when $var has value $value
+      ( $var === $value ? 'selected ' : '' ) .
+      'value="' . $value . '">' . $text .
+      '</option>' . PHP_EOL;
+  }
+
+}
