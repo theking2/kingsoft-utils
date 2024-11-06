7297a51923267bc39edebe5e432a2615814e0f8f
theking2 <theking2@king.ma>
2024-11-06 16:12:59 +0100

    add sample config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+// copy this file to root of your project
+const ROOT = __DIR__ . '/';
+
+// add your project settings here
+const SETTINGS = [ 
+    'db' => [ 
+        'hostname' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'test',
+    ],
+];
